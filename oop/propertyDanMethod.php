27b220f23f64c02fc2fc37960d76bad87c6ed942
AzharLuthfi <azhar.luthfiadi@gmail.com>
2024-10-10 08:47:08 +0700

    Video 3 Property dan Method

---
+++ oop/propertyDanMethod.php
@@ -0,0 +1,32 @@
+<?php
+
+class buku
+{
+
+    public $judul = "judul",
+    $pengarang = "pengarang",
+    $penerbit = "penerbit",
+    $harga = 0;
+
+    //method
+    public function getLabel()
+    {
+        return $this->judul;
+    }
+
+
+}
+
+// // buat objek buku
+$buku1 = new buku();
+
+// // mengisi properti class buku
+$buku1->judul = "laskar pelangi";
+$buku1->pengarang = "Asmarandana";
+$buku1->penerbit = "Puspa Indah";
+$buku1->harga = "200000";
+
+// // menmpilkan data dilayar
+var_dump($buku1)
+
+    ?>
