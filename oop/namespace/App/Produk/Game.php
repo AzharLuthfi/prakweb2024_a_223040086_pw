6bb13b20e02fcdb3d54f51a529debd2bc57afb28
AzharLuthfi <azhar.luthfiadi@gmail.com>
2024-10-11 19:15:26 +0700

    Video 17 NameSpace

---
+++ oop/namespace/App/Produk/Game.php
@@ -0,0 +1,20 @@
+<?php
+class Game extends produk implements Infoproduk
+{
+    public $waktuMain;
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga, $waktuMain);
+        $this->waktuMain = $waktuMain;
+    }
+    public function getInfoProduk()
+    {
+        $str = "game : " . $this->getInfo() . " - {$this->waktuMain} jam";
+        return $str;
+    }
+    public function getInfo()
+    {
+        $str = "{$this->judul}|{$this->getLabel()}  {$this->harga} ";
+        return $str;
+    }
+}
